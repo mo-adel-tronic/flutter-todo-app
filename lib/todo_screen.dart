e26add8cc9648ba96f57530368367ffa053ba1b7
mo-adel-tronic <mo.adel.developer@gmail.com>
2024-03-30 22:39:43 +0200

    add show Bottom Sheet Action to floating button

--- lib/todo_screen.dart
+++ lib/todo_screen.dart
@@ -10,11 +10,55 @@ class TodoScreen extends StatelessWidget {
   Widget build (BuildContext context) {
     return Scaffold(
       backgroundColor: mainColor,
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(context: context, 
+          isScrollControlled: true,
+          builder: (context) {
+            return SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom,
+                left: 15, right: 15),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Add Task', 
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: mainColor
+                      ),),
+                    ),
+                    const TextField(
+                      autofocus: true,
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20,),
+                    TextButton(
+                    onPressed: null,
+                    style: TextButton.styleFrom(
+                      backgroundColor: mainColor,
+                      shape: BeveledRectangleBorder()
+                    ),
+                    child: const Text('Add Task', style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),),
+                    )
+                  ],
+                ),
+              ),
+            );
+          });
+        },
         backgroundColor: secColor,
         foregroundColor: Colors.white,
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: Padding(
